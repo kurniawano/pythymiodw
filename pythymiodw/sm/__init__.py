b885cd1b3c98ccafd3d6f24f9b547c30decaa88a
Oka Kurniawan <kurniawano@ieee.org>
2017-09-18 10:15:44 +0800

    Fix sm module.

--- pythymiodw/sm/__init__.py
+++ pythymiodw/sm/__init__.py
@@ -13,18 +13,20 @@ def set_base_simulator():
 
 class ThymioSM(base_class):
     def __init__(self,MySM):
-	Thymio.__init__(self)
+	base_class.__init__(self)
 	self.behaviour=MySM
 	self.behaviour.start()
 	self.input=Input()
 
     def main_loop(self):
-	prox_data=self.get_prox_sensors_val()
-	temp_data=self.get_temperature()
-	acc_data=self.get_accelerometer()
-	self.input.proximity=copy.deepcopy(prox_data)
-	self.input.temperature=copy.deepcopy(temp_data)
-	self.input.accelerometer=copy.deepcopy(acc_data)
+	prox_horizontal=self.get_prox_horizontal()
+	prox_ground=self.get_prox_ground()
+	temperature=self.get_temperature()
+	accelerometer=self.get_accelerometer()
+	self.input.prox_horizontal=copy.deepcopy(prox_horizontal)
+	self.input.prox_ground=copy.deepcopy(prox_ground)
+	self.input.temperature=copy.deepcopy(temperature)
+	self.input.accelerometer=copy.deepcopy(accelerometer)
 	output=self.behaviour.step(self.input)
 	self.move(output)
 	return True
