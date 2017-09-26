defbb64d1db577dbd675f3743a499c5562cab76d
Oka Kurniawan <kurniawano@ieee.org>
2017-09-26 12:42:55 +0800

    fix space io module.

--- pythymiodw/io/__init__.py
+++ pythymiodw/io/__init__.py
@@ -7,25 +7,25 @@ class Action(object):
 
     
     def __init__(self, fv=0.0, rv=0.0):
-	self.fv=fv
-	self.rv=rv
-	self.convert()
+        self.fv=fv
+        self.rv=rv
+        self.convert()
 
     def convert(self):
-	self.leftv=self.fv-0.5*self.W*self.rv
-	self.leftv=(500/0.2)*self.leftv
-	self.rightv=self.fv+0.5*self.W*self.rv
-	self.rightv=(500/0.2)*self.rightv
+        self.leftv=self.fv-0.5*self.W*self.rv
+        self.leftv=(500/0.2)*self.leftv
+        self.rightv=self.fv+0.5*self.W*self.rv
+        self.rightv=(500/0.2)*self.rightv
 
 class Input(object):
     def __init__(self):
-	self.prox_horizontal=[0.0 for i in range(7)]
-	self.prox_ground=ProxGround()
-	self.temperature=0.0
-	self.accelerometer=[0.0,0.0,0.0]
+        self.prox_horizontal=[0.0 for i in range(7)]
+        self.prox_ground=ProxGround()
+        self.temperature=0.0
+        self.accelerometer=[0.0,0.0,0.0]
 
 class ProxGround(object):
     def __init__(self,delta=[0.0,0.0],reflected=[0.0,0.0],ambiant=[0.0,0.0]):
         self.delta=delta
-	self.reflected=reflected
-	self.ambiant=ambiant
+        self.reflected=reflected
+        self.ambiant=ambiant
