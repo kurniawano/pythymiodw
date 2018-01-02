0cca1dfe010d6895aeb091624905ce49e59a8cbc
Oka Kurniawan <kurniawano@ieee.org>
2018-01-02 13:46:02 +0800

    add button reading and example.

---
+++ examples/testbutton.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import sys
+import time
+#sys.path.append('..')
+from pythymiodw import ThymioReal
+
+m = ThymioReal()
+m.init_read()
+while True:
+    print('color top 1')
+    m.leds_top(0,0,32)
+    m.sleep(0.5)
+    f=m.button_forward
+    b=m.button_backward
+    if b:
+        break
+    if f:
+        print('forwad')
+m.quit()
+
--- pythymiodw/__init__.py
+++ pythymiodw/__init__.py
@@ -27,8 +27,8 @@ class Thymio:
     You never need to instantiate this class directly. Use ThymioReal or ThymioSim instead.
     """
     def __init__(self):
-    """Constructor need not any argument.
-    """
+        """Constructor need not any argument.
+        """
         self.open()
         self._prox_horizontal=[0,0,0,0,0,0,0]
         self._prox_ground_delta=[0,0]
@@ -36,14 +36,20 @@ class Thymio:
         self._prox_ground_ambiant=[0,0]
         self._accelerometer=[0,0,0]
         self._temperature=0.0
+        self._button_center=0
+        self._button_left=0
+        self._button_right=0
+        self._button_forward=0
+        self._button_backward=0
+   
         self.off_leds()
         self.off_sounds()
 
     def run(self):
-    """A method to start the robot. You need not call this directly. It is called by init_read() method.
+        """A method to start the robot. You need not call this directly. It is called by init_read() method.
 
-    This method calls _run() which is implemented in ThymioReal and ThymioSim.
-    """
+        This method calls _run() which is implemented in ThymioReal and ThymioSim.
+        """
         try:
             self._run()
         except Exception as inst:
@@ -54,101 +60,106 @@ class Thymio:
             self.quit()
 
     def _run(self):
-    """Internal method to run the robot. This is called by run() and implemented by ThymioReal and ThymioSim.
-    """
+        """Internal method to run the robot. This is called by run() and implemented by ThymioReal and ThymioSim.
+        """
         pass
 
     def init_read(self):
-    """Method to start running the robot in a thread. It calls run().
-    """
+        """Method to start running the robot in a thread. It calls run().
+        """
         self.thread=Thread(target=self.run)
         self.thread.start()
 
     def open(self):
-    """Method to open the connection. Implemented in the subclass.
-    """
+        """Method to open the connection. Implemented in the subclass.
+        """
         pass    
 
     def close(self):
-    """Method to be called when closing. You need not call this directly. ThymioReal and ThymioSim implements this.
-    """
+        """Method to be called when closing. You need not call this directly. ThymioReal and ThymioSim implements this.
+        """
         print('closing.')
         time.sleep(2)
 
     def main_loop(self):
-    """Main loop to be called every time stelp.
+        """Main loop to be called every time stelp.
 
-    The main loop simply reads the various sensors and returns True.
-    """
+        The main loop simply reads the various sensors and returns True.
+        """
         self.get_prox_horizontal()
         self.get_prox_ground()
         self.get_temperature()
         self.get_accelerometer()
+        self.get_button_center()
+        self.get_button_left()
+        self.get_button_right()
+        self.get_button_forward()
+        self.get_button_backward()
         return True
 
     def leds_top(self,r=0,g=0,b=0):
-    """Turn on top LEDs. Implemented in sub class.
-    """
+        """Turn on top LEDs. Implemented in sub class.
+        """
         pass
 
     def leds_circle(self,led0=0, led1=0, led2=0, led3=0, led4=0, led5=0, led6=0, led7=0):
-    """Turn on circle LEDs. Implemented in sub class.
-    """
+        """Turn on circle LEDs. Implemented in sub class.
+        """
         pass
 
     def leds_buttons(self,led0=0, led1=0, led2=0, led3=0):
-    """Turn on LEDs on buttons. Implemented in sub class.
-    """
+        """Turn on LEDs on buttons. Implemented in sub class.
+        """
         pass
 
     def leds_prox_h(self,led0=0, led1=0, led2=0, led3=0, led4=0, led5=0, led6=0, led7=0):
-    """Turn on horizontal proximity sensors' LEDs. Implemented in sub class.
-    """
+        """Turn on horizontal proximity sensors' LEDs. Implemented in sub class.
+        """
         pass
 
     def leds_prox_v(self,led0=0, led1=0, led2=0, led3=0, led4=0, led5=0, led6=0, led7=0):
-    """Turn on vertical proximity sensors' LEDs. Implemented in sub class.
-    """
+        """Turn on vertical proximity sensors' LEDs. Implemented in sub class.
+        """
         pass
 
     def leds_rc(self,led=0):
-    """Turn on RC LED. Implemented in sub class.
-    """
+        """Turn on RC LED. Implemented in sub class.
+        """
         pass
 
     def leds_bottom_left(self,r=0,g=0,b=0):
-    """Turn on LED on bottom left of robot. Implemented in sub class.
-    """
+        """Turn on LED on bottom left of robot. Implemented in sub class.
+        """
         pass
 
     def leds_bottom_right(self,r=0,g=0,b=0):
-    """Turn on LED on bottom right of robot. Implemented in sub class.
-    """
+        """Turn on LED on bottom right of robot. Implemented in sub class.
+        """
         pass
 
     def leds_temperature(self,r=0,b=0):
-    """Turn on temperature sensor's LED of robot. Implemented in sub class.
-    """
+        """Turn on temperature sensor's LED of robot. Implemented in sub class.
+        """
         pass
 
     def leds_sound(self,led=0):
-    """Turn on speaker's LED of robot. Implemented in sub class.
-    """
+        """Turn on speaker's LED of robot. Implemented in sub class.
+        """
         pass
 
     def sound_system(self,n=0):
-    """Turn on the sound for system. Implemented in sub class.
-    """
+        """Turn on the sound for system. Implemented in sub class.
+        """
         pass
 
     def sound_freq(self,hz=0, ds=0):
-    """Turn on sound using specific frequency. Implemented in sub class.
-    """
+        """Turn on sound using specific frequency. Implemented in sub class.
+        """
         pass
 
     def sound_play(self,n=0):
-    """Play recorded sound. Implemented in sub class.
-    """
+        """Play recorded sound. Implemented in sub class.
+        """
         pass
 
     def sound_replay(self,n=0):
@@ -163,88 +174,138 @@ class Thymio:
         print(str(e))
 
     def get_prox_horizontal(self):
-    """Method to get horizontal proxmity sensors. Implemented by sub class.
-    """
+        """Method to get horizontal proxmity sensors. Implemented by sub class.
+        """
         pass
 
     def get_prox_ground_delta(self):
-    """Method to get delta ground proximity sensor. Implemented by sub class.
-    """
+        """Method to get delta ground proximity sensor. Implemented by sub class.
+        """
         pass
 
     def get_prox_ground_reflected(self):
-    """Method to get reflected ground proximity sensor. Implemented by sub class.
-    """
+        """Method to get reflected ground proximity sensor. Implemented by sub class.
+        """
         pass
 
     def get_prox_ground_ambiant(self):
-    """Method to get ambient ground proximity sensor. Implemented by sub class.
-    """
+        """Method to get ambient ground proximity sensor. Implemented by sub class.
+        """
         pass
 
     def get_prox_ground(self):
-    """Method to get all the three ground proximity sensor: delta, reflected, and ambiant.
+        """Method to get all the three ground proximity sensor: delta, reflected, and ambiant.
 
-    Returns: io.ProxGround object.
-    """
+        Returns: io.ProxGround object.
+        """
         self.get_prox_ground_delta()
         self.get_prox_ground_reflected()
         self.get_prox_ground_ambiant()
         return io.ProxGround(delta=self._prox_ground_delta, reflected=self._prox_ground_reflected, ambiant=self._prox_ground_ambiant)
 
     def get_temperature(self):
-    """Method to get temperature sensor data. Implemented by sub class.
-    """
+        """Method to get temperature sensor data. Implemented by sub class.
+        """
         pass
 
     def get_accelerometer(self):
-    """Method to get accelerometer data. Implemented by sub class.
-    """
+        """Method to get accelerometer data. Implemented by sub class.
+        """
+        pass
+
+    def get_button_center(self):
+        """Method to get center button. Implemented by sub class.
+        """
+        pass
+
+    def get_button_left(self):
+        """Method to get left button. Implemented by sub class.
+        """
+        pass
+
+    def get_button_right(self):
+        """Method to get right button. Implemented by sub class.
+        """
+        pass
+
+    def get_button_forward(self):
+        """Method to get forward button. Implemented by sub class.
+        """
+        pass
+
+    def get_button_backward(self):
+        """Method to get backward button. Implemented by sub class.
+        """
         pass
 
     def read_prox_horizontal(self):
-    """Method to read horizontal proximity sensor. Used by property.
-    """
+        """Method to read horizontal proximity sensor. Used by property.
+        """
         return self._prox_horizontal
 
     def read_prox_ground(self):
-    """Method to read ground proximity sensor. Used by property.
-    """
+        """Method to read ground proximity sensor. Used by property.
+        """
         return io.ProxGround(delta=self._prox_ground_delta, reflected=self._prox_ground_reflected, ambiant=self._prox_ground_ambiant)
 
     def read_temperature(self):
-    """Method to read temperature sensor. Used by property.
-    """
+        """Method to read temperature sensor. Used by property.
+        """
         return self._temperature
 
     def read_accelerometer(self):
-    """Method to read accelerometer. Used by property.
-    """
+        """Method to read accelerometer. Used by property.
+        """
         return self._accelerometer
 
+    def read_button_center(self):
+        """Method to read center button. Used by property.
+        """
+        return self._button_center
+
+    def read_button_left(self):
+        """Method to read left button. Used by property.
+        """
+        return self._button_left
+
+    def read_button_right(self):
+        """Method to read right button. Used by property.
+        """
+        return self._button_right
+
+    def read_button_forward(self):
+        """Method to read forward button. Used by property.
+        """
+        return self._button_forward
+
+    def read_button_backward(self):
+        """Method to read backward button. Used by property.
+        """
+        return self._button_backward
+
     def wheels(self,l,r):
-    """Method to set the left and right speed of robot's wheels.
-    """
+        """Method to set the left and right speed of robot's wheels.
+        """
         self._wheels(l,r)
 
     def _wheels(self,l,r):
-    """Internal method called by wheels(). Implemented by sub class.
-    """
+        """Internal method called by wheels(). Implemented by sub class.
+        """
         pass
 
     def halt(self):
-    """Method to stop the robot. 
-    """
+        """Method to stop the robot. 
+        """
         self._halt()
 
     def _halt(self):
-    """Internal method to stop the robot. Implemented by sub class.
-    """
+        """Internal method to stop the robot. Implemented by sub class.
+        """
         pass
     
     def off_leds(self):
-    """Method to turn off all LEDs.
-    """
+        """Method to turn off all LEDs.
+        """
         self.leds_top()
         self.leds_circle()
         self.leds_buttons()
@@ -257,16 +318,16 @@ class Thymio:
         self.leds_sound()
 
     def off_sounds(self):
-    """Method to switch off the sounds.
-    """
+        """Method to switch off the sounds.
+        """
         self.sound_system(-1)
         self.sound_freq()
         self.sound_play(-1)
         self.sound_replay(-1)
 
     def quit(self):
-    """Overall method to stop and switch off everything.
-    """
+        """Overall method to stop and switch off everything.
+        """
         self.halt()
         self.off_leds()
         self.off_sounds()
@@ -274,8 +335,8 @@ class Thymio:
         self.close()
 
     def quit_loop(self):
-    """Method to quit the main loop.
-    """
+        """Method to quit the main loop.
+        """
         pass
 
     # list of Thymio's properties
@@ -283,6 +344,11 @@ class Thymio:
     prox_ground=property(read_prox_ground)
     temperature=property(read_temperature)
     accelerometer=property(read_accelerometer)
+    button_center=property(read_button_center)
+    button_forward=property(read_button_forward)
+    button_backward=property(read_button_backward)
+    button_left=property(read_button_left)
+    button_right=property(read_button_right)
 
 class ThymioReal(Thymio):
     def open(self):
@@ -384,40 +450,70 @@ class ThymioReal(Thymio):
         time.sleep(n)
 
     def prox_horizontal_handler(self, r):
-    """Callback for robot to send horizontal proximity sensor data.
-    """
+        """Callback for robot to send horizontal proximity sensor data.
+        """
         dist=[int(x) for x in r]
         self._prox_horizontal=dist
 
     def prox_ground_delta_handler(self, r):
-    """Callback for robot to send delta ground proximity sensor data.
-    """
+        """Callback for robot to send delta ground proximity sensor data.
+        """
         t=[int(x) for x in r]
         self._prox_ground_delta=t
 
     def prox_ground_reflected_handler(self, r):
-    """Callback for robot to send reflected ground proximity sensor data.
-    """
+        """Callback for robot to send reflected ground proximity sensor data.
+        """
         t=[int(x) for x in r]
         self._prox_ground_reflected=t
 
     def prox_ground_ambiant_handler(self, r):
-    """Callback for robot to send ambient ground proximity sensor data.
-    """
+        """Callback for robot to send ambient ground proximity sensor data.
+        """
         t=[int(x) for x in r]
         self._prox_ground_ambiant=t
 
     def acc_handler(self, r):
-    """Callback for robot to send accelerometer data.
-    """
+        """Callback for robot to send accelerometer data.
+        """
         t=[int(x) for x in r]
         self._accelerometer=t
 
     def temperature_handler(self, r):
-    """Callback for robot to send temperature sensor data.
-    """
+        """Callback for robot to send temperature sensor data.
+        """
         t=[int(x) for x in r]
         self._temperature=t[0]/10.0
+
+    def button_center_handler(self, r):
+        """Callback for robot to send center button press.
+        """
+        press=[int(x) for x in r]
+        self._button_center=press[0]
+
+    def button_left_handler(self, r):
+        """Callback for robot to send left button press.
+        """
+        press=[int(x) for x in r]
+        self._button_left=press[0]
+
+    def button_right_handler(self, r):
+        """Callback for robot to send right button press.
+        """
+        press=[int(x) for x in r]
+        self._button_right=press[0]
+
+    def button_forward_handler(self, r):
+        """Callback for robot to send forward button press.
+        """
+        press=[int(x) for x in r]
+        self._button_forward=press[0]
+
+    def button_backward_handler(self, r):
+        """Callback for robot to send backward button press.
+        """
+        press=[int(x) for x in r]
+        self._button_backward=press[0]
 
     def get_variables_error(self, e):
         Thymio.get_variables_error(self,e)
@@ -447,6 +543,35 @@ class ThymioReal(Thymio):
         self.network.GetVariable(self.device,"acc", reply_handler=self.acc_handler, error_handler=self.get_variables_error)
         return self._accelerometer
 
+    def get_button_center(self):
+        """Method to get center button. 
+        """
+        self.network.GetVariable(self.device,"button.center", reply_handler=self.button_center_handler, error_handler=self.get_variables_error)
+        return self._button_center
+
+    def get_button_left(self):
+        """Method to get left button. 
+        """
+        self.network.GetVariable(self.device,"button.left", reply_handler=self.button_left_handler, error_handler=self.get_variables_error)
+        return self._button_left
+
+    def get_button_right(self):
+        """Method to get right button. 
+        """
+        self.network.GetVariable(self.device,"button.right", reply_handler=self.button_right_handler, error_handler=self.get_variables_error)
+        return self._button_right
+
+    def get_button_forward(self):
+        """Method to get forward button. 
+        """
+        self.network.GetVariable(self.device,"button.forward", reply_handler=self.button_forward_handler, error_handler=self.get_variables_error)
+        return self._button_forward
+
+    def get_button_backward(self):
+        """Method to get backward button. 
+        """
+        self.network.GetVariable(self.device,"button.backward", reply_handler=self.button_backward_handler, error_handler=self.get_variables_error)
+        return self._button_forward
 
     def _wheels(self,l,r):
         self.set(self.device, "motor.left.target",[l])
