410d0765566438be0f0edca7e675411af815be14
Oka Kurniawan <kurniawano@ieee.org>
2018-01-05 08:59:35 +0800

    Modify doc and add example on sm.

--- doc/getstart.rst
+++ doc/getstart.rst
@@ -7,9 +7,9 @@ Installation and Upgrade
 --------------------------
 
 To install pythymiodw library, type the following in a shell::
-	> pip3 install pythymiodw
+	> sudo pip3 install pythymiodw
 
 If you already have pythymiodw installed, you can upgrade by typing::
-	> pip3 install pythymiodw --upgrade
+	> sudo pip3 install pythymiodw --upgrade
 
 
---
+++ examples/smbrainsim.py
@@ -0,0 +1,28 @@
+from pythymiodw import *
+from pythymiodw import io
+from pythymiodw.sm import *
+from libdw import sm
+from boxworld import *
+
+class MySMClass(sm.SM):
+    start_state=None
+    def get_next_values(self, state, inp):
+        ground=inp.prox_ground.delta
+        left=ground[0]
+        right=ground[1]
+        print(left,right)
+        #print(inp.prox_ground.delta)
+        #print(inp.prox_ground.reflected)
+        #print(inp.prox_ground.ambiant)
+                    
+        return state, io.Action(fv=0.05, rv=0.1)
+
+MySM=MySMClass()
+
+############################
+
+m=ThymioSMSim(MySM,thymio_world)
+try:
+    m.start()
+except KeyboardInterrupt:
+    m.stop()
