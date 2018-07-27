a9ae5c42e50b1f13306c0104da29ae441003af24
Oka Kurniawan <kurniawano@ieee.org>
2018-07-27 15:21:55 +0800

    modify example for pyrolite.

--- examples/testpyrolite.py
+++ examples/testpyrolite.py
@@ -4,7 +4,9 @@ import time
 thymio = ThymioMR()
 print('Changing wheel speed.')
 thymio.wheels(100,101)
-time.sleep(10)
+print('Now run C# project to change prox_horizontal and prox_ground.')
+print('You have 15 sec before this code starts to read.')
+time.sleep(15)
 print(thymio.prox_horizontal)
 pg = thymio.prox_ground
 print(pg.delta, pg.ambiant, pg.reflected)
