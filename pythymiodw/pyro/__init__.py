cbfa5d916585524212193f476db4affa38eed5a8
Oka Kurniawan <kurniawano@ieee.org>
2018-07-27 13:58:43 +0800

    Add sleep, and prox_ground, prox_horizontal.

--- pythymiodw/pyro/__init__.py
+++ pythymiodw/pyro/__init__.py
@@ -3,11 +3,14 @@ import Pyro4
 import subprocess
 import signal
 from pythymiodw import ThymioSimMR
+import time
+from pythymiodw.io import ProxGround
 
 
 class ThymioMR():
 	def __init__(self):
 		self.pyro4daemon_proc=subprocess.Popen(['python -m pythymiodw.pyro.__main__'], stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)    
+		time.sleep(2)
 		self.robot = Pyro4.Proxy('PYRONAME:pythymiodw.thymiosimmr')
 
 	def quit(self):
@@ -19,5 +22,19 @@ class ThymioMR():
 
 	def get_wheels(self):
 		return self.robot.leftv, self.robot.rightv
+
+	def sleep(self, sec):
+		time.sleep(sec)
+
+	@property
+	def prox_horizontal(self):
+		return self.robot.prox_horizontal
+
+	@property
+	def prox_ground(self):
+		delta, ambiant, reflected = self.robot.prox_ground
+		return ProxGround(delta, ambiant, reflected)
+	
+	
 
         
