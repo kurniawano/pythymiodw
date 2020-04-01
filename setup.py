5d6cd5b7feb71702eabf1d3fbd7d97fba344e1c9
Oka Kurniawan <kurniawano@ieee.org>
2020-04-01 15:39:28 +0800

    update version no.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pythymiodw',
-    version='2.4.0',
+    version='2.4.1',
     description='Python library for Thymio used in Digital World class',
     long_description=long_description,
     url='https://github.com/kurniawano/pythymiodw',
